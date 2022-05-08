858971d2bc689e5ff6c3662d98aad09bdde42ba6
NicolaLovo <nicolalovo@gmail.com>
2022-05-08 20:03:19 +0200

    comments

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 //guide:https://itnext.io/step-by-step-building-and-publishing-an-npm-typescript-package-44fe7164964c
+
+//npm publish to publish to npm --> DO NOT USE IT
+//npm version patch to bump the version
+
 import Stylesheet from "./Stylesheet";
 export const Greeter = (name: string) => `Hello ${name}`;
 export default Stylesheet;
