b805360fa2dbe54b8b3976d2f838a1227d32a61d
NicolaLovo <nicolalovo@gmail.com>
2022-05-08 20:01:52 +0200

    first stylesheet

---
+++ src/Stylesheet.ts
@@ -0,0 +1,10 @@
+interface CSSProperties {
+	[key: string]: React.CSSProperties;
+}
+
+class Stylesheet {
+	static create(styleData: CSSProperties): CSSProperties {
+		return styleData;
+	}
+}
+export default Stylesheet;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+//guide:https://itnext.io/step-by-step-building-and-publishing-an-npm-typescript-package-44fe7164964c
+import Stylesheet from "./Stylesheet";
 export const Greeter = (name: string) => `Hello ${name}`;
+export default Stylesheet;
+// Stylesheet.create({
+//    greeter: {
+//       color: "red"
+//    },
+// });
